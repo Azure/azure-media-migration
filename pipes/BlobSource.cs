69185f9dd40dcf6e80697dc6196a4d5be75673af
Prakash Duggaraju <duggaraju@gmail.com>
2023-08-23 17:40:44 -0700

    Only create pipes when pipes are enabled. Remove duplication between BlobStream and BlobSource. Refactor the pipe code. Fixes #151

--- pipes/BlobSource.cs
+++ pipes/BlobSource.cs
@@ -1,5 +1,7 @@
-﻿using AMSMigrate.Decryption;
+﻿
+using AMSMigrate.Decryption;
 using Azure.ResourceManager.Media.Models;
+using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using Azure.Storage.Blobs.Specialized;
 using FFMpegCore.Pipes;
@@ -7,7 +9,7 @@ using Microsoft.Extensions.Logging;
 
 namespace AMSMigrate.Pipes
 {
-    internal class BlobSource : IPipeSource
+    sealed class BlobSource : IPipeSource
     {
         private static readonly IReadOnlyDictionary<string, string> ExtensionToFormatMap = new Dictionary<string, string>
         {
@@ -15,20 +17,54 @@ namespace AMSMigrate.Pipes
             { ".vtt", "webvtt" }
         };
 
-        private readonly BlockBlobClient _blobClient;
-        private readonly StorageEncryptedAssetDecryptionInfo? _decryptInfo;
         private readonly ILogger _logger;
+        private readonly BlockBlobClient _blob;
+        private readonly StorageEncryptedAssetDecryptionInfo? _decryptInfo;
 
-        public BlobSource(BlockBlobClient blobClient, StorageEncryptedAssetDecryptionInfo? decryptInfo, ILogger logger)
+        public BlobSource(BlobContainerClient container, string blobName, StorageEncryptedAssetDecryptionInfo? decryptInfo, ILogger logger) :
+            this(container.GetBlockBlobClient(blobName), decryptInfo, logger)
         {
-            _blobClient = blobClient;
-            _decryptInfo = decryptInfo;
+        }
+
+        public BlobSource(BlockBlobClient blob, StorageEncryptedAssetDecryptionInfo? decryptInfo, ILogger logger)
+        {
+            _blob = blob;
             _logger = logger;
+            _decryptInfo = decryptInfo;
+        }
+
+        public async Task DownloadAsync(Stream stream, CancellationToken cancellationToken)
+        {
+            _logger.LogDebug("Begin downloading {name}", _blob.Name);
+
+            using var aesTransform = AssetDecryptor.GetAesCtrTransform(_decryptInfo, _blob.Name, false);
+
+            if (aesTransform != null)
+            {
+                await AssetDecryptor.DecryptTo(aesTransform, _blob, stream, cancellationToken);
+            }
+            else
+            {
+                await _blob.DownloadToAsync(stream, cancellationToken: cancellationToken);
+            }
+
+            _logger.LogDebug("Finished download of {name}", _blob.Name);
+        }
+
+        public async Task DownloadAsync(string filePath, CancellationToken cancellationToken)
+        {
+            using var stream = File.OpenWrite(filePath);
+            await DownloadAsync(stream, cancellationToken);
+        }
+
+        public async Task UploadAsync(Stream stream, CancellationToken cancellationToken)
+        {
+            BlobContentInfo info = await _blob.UploadAsync(stream, cancellationToken: cancellationToken);
         }
 
         public string GetStreamArguments()
         {
-            var extension = Path.GetExtension(_blobClient.Name);
+            var extension = Path.GetExtension(_blob.Name);
             if (!ExtensionToFormatMap.TryGetValue(extension, out var format))
             {
                 format = "mp4"; // fallback to mp4.
@@ -38,40 +74,7 @@ namespace AMSMigrate.Pipes
 
         public async Task WriteAsync(Stream outputStream, CancellationToken cancellationToken)
         {
-            _logger.LogDebug("Begin downloading track: {name}", _blobClient.Name);
-            var options = new BlobDownloadOptions
-            {
-                ProgressHandler = new Progress<long>(
-                    progress =>
-                    {
-                        _logger.LogTrace("Download progress of blob {blob} is {progress}", _blobClient.Name, progress);
-                    })
-            };
-            using BlobDownloadStreamingResult result = await _blobClient.DownloadStreamingAsync(options, cancellationToken);
-            try
-            {
-                await result.Content.CopyToAsync(outputStream, cancellationToken);
-                _logger.LogDebug("Finished downloading track: {name}", _blobClient.Name);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Failed to download {blob}", _blobClient.Name);
-                throw;
-            }
-        }
-
-        public async Task DownloadAsync(string filePath, CancellationToken cancellationToken)
-        {
-            using var aesTransform = AssetDecryptor.GetAesCtrTransform(_decryptInfo, _blobClient.Name, false);
-
-            if (aesTransform != null)
-            {
-                await AssetDecryptor.DecryptTo(aesTransform, _blobClient, filePath, cancellationToken);
-            }
-            else
-            {
-                await _blobClient.DownloadToAsync(filePath, cancellationToken);
-            }
+            await DownloadAsync(outputStream, cancellationToken);
         }
     }
 }
