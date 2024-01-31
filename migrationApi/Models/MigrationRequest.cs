77fa7945ea3edc732830892c0f541f0fa04ccebb
Eloy Vega <eloy.vega@southworks.com>
2024-01-31 13:44:10 -0300

    fix json parse

--- migrationApi/Models/MigrationRequest.cs
+++ migrationApi/Models/MigrationRequest.cs
@@ -4,19 +4,14 @@ namespace migrationApi.Models
 {
     public class MigrationRequest
     {
-        [JsonPropertyName("subscriptionId")]
         public string SubscriptionId { get; set; }
 
-        [JsonPropertyName("resourceGroup")]
         public string ResourceGroup { get; set; }
 
-        [JsonPropertyName("mediaServiceName")]
         public string MediaServiceName { get; set; }
 
-        [JsonPropertyName("targetStorageAccountName")]
         public string TargetStorageAccountName { get; set; }
 
-        [JsonPropertyName("assetName")]
         public string AssetName { get; set; }
     }
 }
