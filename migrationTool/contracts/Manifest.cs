8caf93331e574acfb41e15959b7d32425850aae6
Carsten Schuette <schuettecarsten@googlemail.com>
2024-05-29 19:36:48 +0200

    Fix an exception that is thrown when the manifest is valid but does not contain trackId data (#248)

--- migrationTool/contracts/Manifest.cs
+++ migrationTool/contracts/Manifest.cs
@@ -51,9 +51,9 @@ namespace AMSMigrate.Contracts
         // Check if the track is stored as one file per fragment.
         public bool IsMultiFile => string.IsNullOrEmpty(Path.GetExtension(Source));
 
-        public uint TrackID => uint.Parse(Parameters.Single(p => p.Name == "trackID").Value);
+        public uint TrackID => uint.Parse(Parameters?.SingleOrDefault(p => p.Name == "trackID")?.Value ?? "1");
 
-        public string TrackName => Parameters.SingleOrDefault(p => p.Name == "trackName")?.Value ?? Type.ToString().ToLower();
+        public string TrackName => Parameters?.SingleOrDefault(p => p.Name == "trackName")?.Value ?? Type.ToString().ToLower();
     }
 
     public class VideoTrack : Track
